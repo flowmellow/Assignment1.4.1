b0d33622ad3591ee552a10c73458bae9f45d3484
flowmellow <michael.floresmmt@gmail.com>
2024-05-09 11:38:50 -0400

    Add project files.

---
+++ Assignment1.4.1/Program.cs
@@ -0,0 +1,34 @@
+﻿//Create a structure named “Point” and 2 data members: X and Y coordinate. Declare 2 points: P1 and P2.
+//Determine if P2 is to the right or left of P1 or on same axis , by comparing the x xoordinates.
+//( if p1.x is more than p2.x , it is to the right )
+
+using System.Runtime.InteropServices;
+
+namespace Assignment1._4._1
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Point point1 = new Point();
+            Point point2 = new Point();
+
+            if (point1.x == point2.x)
+            {
+                Console.WriteLine("The points are on the same axis:");
+            }
+            else
+            {
+                Console.WriteLine("The points are not on the same axis");
+            }
+        }
+        public struct Point
+        {
+            double x;
+            double y;
+
+        }
+
+    }
+}
